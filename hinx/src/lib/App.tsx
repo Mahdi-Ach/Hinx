a891e9b60a3a5b3df28a19fd66d29f62c3ff6244
Mahdi-Ach <achbabmahdi120@gmail.com>
2024-08-06 04:52:26 +0100

    modify package name

---
+++ hinx/src/lib/App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import TabDemo from './Tabs/TabDemo';
+import AccordionDemo from './Accordion/AccordionDemo';
+
+function App() {
+  
+  return (
+    <>
+      <TabDemo />
+      <TabDemo />
+      <AccordionDemo />
+    </>
+  );
+}
+
+export default App;
